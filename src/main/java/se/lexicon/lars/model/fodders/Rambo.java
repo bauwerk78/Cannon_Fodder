87bf1a24b4cad14d08bde6670fba0a1ced891540
bauwerk78 <57097401+bauwerk78@users.noreply.github.com>
2019-12-09 21:11:52 +0100

    Rambo objects moving sideways, makes it much harder.

--- src/main/java/se/lexicon/lars/model/fodders/Rambo.java
+++ src/main/java/se/lexicon/lars/model/fodders/Rambo.java
@@ -5,30 +5,64 @@ import se.lexicon.lars.model.GameRound;
 
 public class Rambo extends Fodder {
 
+    private double timeCounter = 0;
+    private boolean movingRight;
+    private boolean movingLeft;
+
     public Rambo(double speed, String imagePath) {
         super(speed, imagePath);
+    }
+
+//TODO all beneath this.
+    public boolean delayer() {
+        int secondsToDelay = 2;
+        if (timeCounter < secondsToDelay) {
+            timeCounter += GameRound.elapsedTime;
+            return true;
+        }
+        timeCounter = 0;
+        movingLeft = false;
+        movingRight = false;
+        return false;
+    }
+
+    public void randomXDirection() {
+        int randomXDirection = rand.nextInt(2);
+        if (!delayer()) {
+            if (randomXDirection == 0) {
+                movingLeft = true;
+            }
+            if(randomXDirection == 1){
+                movingRight = true;
+            }
+        }
     }
 
     @Override
     protected void moveFodder() {
         setPositionY(getPositionY() + getFodderSpeed() * GameRound.elapsedTime);
-        int randomXDirection = rand.nextInt(2);
-        //TODO fix the jittery movement back and forth.
-        if(randomXDirection == 0) {
+        randomXDirection();
+        if (movingLeft && delayer()) {
+            //System.out.println("going left");
             setPositionX(getPositionX() - (getFodderSpeed() * GameRound.elapsedTime));
         }
-        if(randomXDirection == 1) {
+        if (movingRight && delayer()) {
+            //System.out.println("going right");
             setPositionX(getPositionX() + (getFodderSpeed() * GameRound.elapsedTime));
         }
-        if(getPositionX() < 0) {
+        if (getPositionX() < 0) {
             setPositionX(0);
         }
-        if(getPositionX() + getImageWidth() > windowWidth) {
+        if (getPositionX() + getImageWidth() > windowWidth) {
             setPositionX(windowWidth - getImageWidth());
         }
 
 
     }
+
+
+
+
 
 
 }
